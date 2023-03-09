fc7e65caf3a2434fb04a9a0e60dd80e1ced9eed4
김용민 <dydals3440@gmail.com>
2023-03-09 15:59:46 +0900

    study: router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Products from "./AppProduct/Products";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Products />
+    <App />
   </React.StrictMode>
 );
 
