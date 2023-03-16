df35ce442379f1eeeec16577fd57c016d7cd19bb
김용민 <dydals3440@gmail.com>
2023-03-16 18:00:51 +0900

    React: Studying react-query

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
+import React from "react";
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NotFound from "./router/pages/NotFound";
-import Videos from "./router/pages/Videos";
-import Home from "./router/pages/Home";
-import Root from "./router/pages/Root";
-import VideoDetail from "./router/pages/VideoDetail";
+import Mainproducts from "./Query/components/Mainproducts";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "videos", element: <Videos /> },
-      { path: "videos/:videoId", element: <VideoDetail /> },
-    ],
-  },
-]);
-
-function App() {
-  // 위에있는 router로 우산을 씌워줌
-  return <RouterProvider router={router} />;
+export default function App() {
+  return <Mainproducts />;
 }
-
-export default App;
